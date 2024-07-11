9b824acdb2dd1cd25f01204fadd27dc67a638896
rigo234xd <143448359+rigo234xd@users.noreply.github.com>
2024-07-10 20:15:01 -0400

    Add files via upload

--- ticket.py
+++ ticket.py
@@ -1,4 +1,5 @@
 import json
+import os
 class ticket:
     
     nombreConductor: str
@@ -10,25 +11,22 @@ class ticket:
     def __init__(self, nombreConductor, patenteVehiculo, duracion, precio, rut):
         
         self.nombreConductor = nombreConductor
+        self.rut = rut
         self.patenteVehiculo = patenteVehiculo
         self.duracion = duracion
         self.precio = precio
-        self.rut = rut
         #aca tendria que ir el lugar en la amtriz que falta hacer aun
         
-    def Crear_Boleta(){
+    def Crear_Boleta(self):
         ticket_aux = {
-            "Rut": ticket.rut,
-            "Nombre": ticket.nombre,
-            "Patente": ticket.patenteVehiculo,
-            "tiempo": ticket.duracion,
-            "valor": ticket.precio,
-            #"lugar": ticket.lugar porque la matriz aun no esta hecha
+            "Nombre": self.nombreConductor,
+            "Rut": self.rut,
+            "Patente": self.patenteVehiculo,
+            "tiempo": self.duracion,
+            "valor": self.precio,
         }
         ticket_json = json.dumps(ticket_aux)
-        with open("ticket.json", "w") as json_file:
+        os.makedirs("Boletas", exist_ok=True)
+        file_path = os.path.join("Boletas", "ticket.json")
+        with open(file_path, "w") as json_file:
             json_file.write(ticket_json)
-    }
-    #preguntar al profe si en la funcion open es mejor el "w" o el "a" o el "r+" no se cual estaria mejor
-#preguntar igual una forma de que las boletas vallan guardandose con distintos numeros o si el mismo explorador de archivos va creando
-#distintos numeros en los ticket(como cuando hay un mismo archivo el ordenador hace archivo archivo(2)
